ec9e40377d53c199883475964697458a73d063e3
James <jigneshrathodwkr1986@gmail.com>
2021-06-30 20:10:40 +0530

    Fix

--- sale_multi_warehouse/__manifest__.py
+++ sale_multi_warehouse/__manifest__.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Sale Multi Delivery by Warehouse',
-    'version': '13.0',
+    'version': '14.0',
     'author': 'jignesh@test.com',
     "category": "Warehouse",
     "depends": ['sale_stock', 'sale_management'],
     'summary': 'Sale multi warehouse | Different Warehouse on sale orderline | Sale Order multiple warehouse | Deliver product from multiple warehouses set warehouse on sale order line ',
     'description': """Sale Order Picking Warehouse Multiple Warehouse on sale order line ship from multiple warehouse outgoing shipment
-    
     'data': [
     'views/sale_view.xml'],
-    'price': 10.0,
-    'currency': "EUR",
     "images":[],
     'application': True,
     'installable': True,
