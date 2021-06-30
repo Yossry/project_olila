9b8a4a838cc88bf69261205f16749b31d9542940
James <jigneshrathodwkr1986@gmail.com>
2021-06-30 21:49:19 +0530

    Fix issue

--- pw_sale_multi_warehouse/__manifest__.py
+++ pw_sale_multi_warehouse/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Sale Multi Delivery by Warehouse',
-    'version': '14.0.1.0',
-    'author': 'jignesh@test.com',
-    "category": "Sales",
+    'name': 'Sale Multi Warehouse Delivery',
+    'version': '14.0',
+    'author': 'Preciseways IT Solutions',
+    "category": "Warehouse",
     "depends": ['sale_stock', 'sale_management'],
     'summary': 'Sale multi warehouse | Different Warehouse on sale orderline | Sale Order multiple warehouse | Deliver product from multiple warehouses set warehouse on sale order line ',
     'description': """Sale Order Picking Warehouse Multiple Warehouse on sale order line ship from multiple warehouse outgoing shipment
+    """,
     'data': [
     'views/sale_view.xml'],
     "images":[],
